ebf88c57e51204007fb9e5094c04e81ee6e990e9
LeonERK <rrosales2023@udec.cl>
2025-09-26 15:46:37 -0300

    Orden

---
+++ Java-Spring/Actividades Core/Algoritmos Basicos/Algoritmos.java
@@ -0,0 +1,9 @@
+public class Algoritmos {
+    public static void main(String[] args) {
+        int numero = 4;
+    }
+    public static boolean esPar(int numero) {
+        return numero % 2 == 0;
+    }
+
+}
